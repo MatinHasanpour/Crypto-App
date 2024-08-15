025ed54136d437fde070d0bb5495279577e44ed8
MatinHasanpour <101702136+MatinHasanpour@users.noreply.github.com>
2024-08-15 12:40:08 +0330

    Crypto App

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import HomePage from "./components/templates/HomePage";
+
+function App() {
+  return (
+    <>
+      <h1>Botostart</h1>
+      <HomePage />
+    </>
+  );
+}
+
+export default App;
